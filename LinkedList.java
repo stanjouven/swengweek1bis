5171eea9bbfed8482bdeee54d5c3bff69af2d661
stanislas jouven <stanislas.jouven@flaminem.com>
2018-09-30 16:38:13 +0200

    Add printing

--- LinkedList.java
+++ LinkedList.java
@@ -10,4 +10,11 @@ class LinkedList{
             data = d;
         }
     }
+    public void printList(){  
+    Node n = head; 
+    while (n != null){ 
+        System.out.print(n.data+" "); 
+        n = n.next;
+    }
+}  
 }
